89b5f93e724f412f6ec63495a69c137a6fe24cfb
abrarnb <abrar.pv@nestorbird.com>
2024-02-24 01:44:12 +0530

    change on_update to on_submit

--- nizwa/nizwa/hooks/doc_events/contract.py
+++ nizwa/nizwa/hooks/doc_events/contract.py
@@ -6,7 +6,7 @@ def autoname(doc, method):
     if doc.party_type == 'Customer':
         doc.name = make_autoname(doc.custom_contract_no)
 
-def on_update(doc, method):
+def on_submit(doc, method):
     if doc.party_type == 'Customer' and doc.is_new():
         price_list_name =  doc.custom_contract_name + doc.custom_contract_no
         if not frappe.db.exist("Price List", price_list_name):
