21cdecb51f690877c00cbd1f71bc0d6921e3b5af
abrarnb <abrar.pv@nestorbird.com>
2024-02-24 01:49:16 +0530

    fix: price list not created

--- nizwa/nizwa/hooks/doc_events/contract.py
+++ nizwa/nizwa/hooks/doc_events/contract.py
@@ -7,11 +7,10 @@ def autoname(doc, method):
         doc.name = make_autoname(doc.custom_contract_no)
 
 def on_submit(doc, method):
-    if doc.party_type == 'Customer' and doc.is_new():
-        price_list_name =  doc.custom_contract_name + doc.custom_contract_no
-        if not frappe.db.exist("Price List", price_list_name):
+    if doc.party_type == 'Customer':
+        if not frappe.db.exist("Price List", doc.name):
             price_list_doc = frappe.new_doc("Price List")
-            price_list_doc.price_list_name = price_list_name
+            price_list_doc.price_list_name = doc.name
             price_list_doc.currency = "OMR"
             price_list_doc.selling = 1
             price_list_doc.append("countries", {
