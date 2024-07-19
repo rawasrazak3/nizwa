ee9561d5bdb6684d899c41b3a6f1ea941779b285
rawasrazak3 <rawasrazak3@gmail.com>
2024-07-19 07:04:16 +0000

    added all custom fields and other minor changes

--- nizwa/nizwa/hooks/doc_events/retension_bonus.py
+++ nizwa/nizwa/hooks/doc_events/retension_bonus.py
@@ -19,17 +19,18 @@ def before_validate(doc, method):
                 doc.bonus_amount = item.rate
 
 def on_submit(doc, method):
-    attendance = frappe.db.get_value("Attendance", {'attendance_date': doc.bonus_payment_date, 'employee': doc.employee}, 'name')
-    if not attendance:
-        attendance_doc = frappe.new_doc("Attendance")
-        attendance_doc.attendance_date = doc.bonus_payment_date
-        attendance_doc.employee = doc.employee
-        attendance_doc.status = 'Present'
-        attendance_doc.company = 'Nizwa Energy SPC'
-        attendance_doc.shift = doc.custom_shift_type
-        attendance_doc.custom_project = doc.custom_project
-        attendance_doc.custom_cost_center = doc.custom_cost_center
-        attendance_doc.custom_retention_bonus = doc.name
-        attendance_doc.save(ignore_permissions=True)
-        attendance_doc.submit()
+    # attendance = frappe.db.get_value("Attendance", {'attendance_date': doc.bonus_payment_date, 'employee': doc.employee}, 'name')
+    # if not attendance:
+    #     attendance_doc = frappe.new_doc("Attendance")
+    #     attendance_doc.attendance_date = doc.bonus_payment_date
+    #     attendance_doc.employee = doc.employee
+    #     attendance_doc.status = 'Present'
+    #     attendance_doc.company = 'Nizwa Energy SPC'
+    #     attendance_doc.shift = doc.custom_shift_type
+    #     attendance_doc.custom_project = doc.custom_project
+    #     attendance_doc.custom_cost_center = doc.custom_cost_center
+    #     attendance_doc.custom_retention_bonus = doc.name
+    #     attendance_doc.save(ignore_permissions=True)
+    #     attendance_doc.submit()
+        pass
 
